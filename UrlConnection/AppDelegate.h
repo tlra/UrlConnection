696573f23400fc5bb6a07926a5464faeb0cfd8f8
涛哥 <taoge@taogedeMacBook-Pro.local>
2015-01-28 13:12:33 +0800

    Initial Commit

---
+++ UrlConnection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UrlConnection
+//
+//  Created by 涛哥 on 15/1/28.
+//  Copyright (c) 2015年 涛哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UrlConnection/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UrlConnection
+//
+//  Created by 涛哥 on 15/1/28.
+//  Copyright (c) 2015年 涛哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
